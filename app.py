b7baaeefb2ae4c728fd1af0139fac5d61ce22d83
rial99 <ratulstephenite99@gmail.com>
2018-06-09 18:42:26 +0530

    authentication added

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate,identity
 
 app = Flask(__name__)
+app.secret_key = 'jose'
 api = Api(app)
+
+jwt = JWT(app,authenticate,identity) # /auth
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self,name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item':None}, 404
+        ##for item in items:
+        ##    if item['name'] == name
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item':item}, 200 if item else 404
+
     def post(self,name):
-        item = {'name':name,'price':12.00}
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "an item with name '{}'already exists. ".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name':name,'price':data['price']}
         items.append(item)
         return item, 201
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
 
-app.run(port=5000)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList,'/items')
+
+app.run(port=5000,debug=True,host='0.0.0.0')
